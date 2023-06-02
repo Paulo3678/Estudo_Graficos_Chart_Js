c3b6e02009e0d5f35f1600247b7c166b2a3dcbbc
Paulo <paulo@agencialumadigital.com.br>
2023-06-02 10:58:41 -0300

    :sparkles: grafico pizza

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const myChart = new Chart(ctx, {});
+function gerarGraficoPizzaContactadoVsNaoContactado() {
+    let canva_pizza = document.getElementById('grafico-pizza');
+    let ctx_pizza = canva_pizza.getContext('2d');
+
+    // DADOS DO GRÁFICO
+    let data = {
+        labels: ['Contactado', 'Não Contactado'],
+        datasets: [{
+            data: [12, 19],
+
+        }]
+    };
+
+    // RENDERIZAR GRAFICO
+    let grafico_pizza_contactados = new Chart(ctx_pizza, {
+        type: 'pie',
+        data: data
+    });
+}
+
+gerarGraficoPizzaContactadoVsNaoContactado();
