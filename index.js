a3f6ef1e06229d36cae56307a2b0fc02fd2abda6
Paulo <paulo@agencialumadigital.com.br>
2023-06-02 11:04:15 -0300

    :sparkles: Grafico linha frequencia

--- index.js
+++ index.js
@@ -11,11 +11,42 @@ function gerarGraficoPizzaContactadoVsNaoContactado() {
         }]
     };
 
-    // RENDERIZAR GRAFICO
+    // RENDERIZAR GRÁFICO
     let grafico_pizza_contactados = new Chart(ctx_pizza, {
         type: 'pie',
         data: data
     });
 }
 
+function gerarGraficoLinhaFrequencia() {
+    let canvas_linha_frequencia = document.getElementById('grafico-linha-frequencia');
+    let ctx = canvas_linha_frequencia.getContext('2d');
+
+    // DADOS DO GRÁFICO
+    let data = {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'], // PARTE INFERIOR
+        datasets: [{
+            label: 'Frequência',
+            data: [10, 15, 7, 12, 9, 11], // DADOS QUE SERÃO APRESENTADOS
+            fill: false
+        }]
+    };
+
+    // RENDERIZAR GRÁFICO
+    let grafico_linha_frequencia = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+
 gerarGraficoPizzaContactadoVsNaoContactado();
+gerarGraficoLinhaFrequencia();
